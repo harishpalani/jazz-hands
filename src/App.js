7072f8a553abc8e5905df689336a97a37fe43837
ManavR123 <manav.rathod@berkeley.edu>
2019-10-26 01:18:22 -0700

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const React = require('react');
+const logo = require('./logo.svg');
+const cv = require('opencv4nodejs');
+
+const styles = {
+  App: {
+    textAlign: "center",
+  },
+
+  AppLogo: {
+    height: "40vmin",
+  },
+
+  AppHeader: {
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white",
+  },
+
+  AppLink: {
+    color: "#09d3ac",
+  },
+
+};
+
+const devicePort = 0;
+const wCap = new cv.VideoCapture(devicePort);
+
+// read frames from capture
+const frame = wCap.read();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={styles.App}>
+      <header style={styles.AppHeader}>
+        <img src={logo} style={styles.AppLogo} alt="logo" />
+        {frame}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          style={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
