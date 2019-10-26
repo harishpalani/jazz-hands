b228ba6b498c98f5b7433975f49c6973bd7dcec9
ManavR123 <manav.rathod@berkeley.edu>
2019-10-26 14:10:17 -0700

    small fixes and directory updates

--- server/app.py
+++ server/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, Response, jsonify, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='public/static', template_folder="public")
 
 @app.route("/")
-def root():
-    return render_template('public/index.html');
+def index():
+    return render_template('index.html')
 
 @app.route('/detect', methods=['POST'])
 def detect():
     img = request.get_json()["image"]
-    return img
+    return jsonify({"note": detection.detect(img)})
 
 
 if __name__ == '__main__':
