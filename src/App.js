3bbb722b821284561b99aa58a11bc70ec07ea519
ManavR123 <manav.rathod@berkeley.edu>
2019-10-27 01:21:21 -0700

    Integrated Dropdown menu to frontend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Webcam from "react-webcam";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.png";
+import Select from "react-dropdown-select";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Nav from "react-bootstrap/Nav";
 const https = require("https");
 
 const styles = {
@@ -20,19 +24,17 @@ const styles = {
   },
 
   AppLogo: {
-    height: "8vmin",
-    width: "8vmin",
-    margin: "16px",
+    height: "4vmin",
+    width: "4vmin",
+    margin: "8px",
   },
 
   Button: {
     alignSelf: "center",
-    width: "600px"
-  },
-
-  Heading: {
-    font: "Jazz LET",
-    marginBottom: "-8px"
+    width: "600px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   Jumbo: {
@@ -47,20 +49,20 @@ const styles = {
 };
 
 function App() {
-  const [calibrate, setCalibrate] = useState(false);
+  const [calibrate, setCalibrate] = useState("false");
   const [note, setNote] = useState("");
   const webcamRef = useRef(null);
    
   const capture = useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
-      if (calibrate) {
+      if (false) {
         httpsPost({"image": imageSrc, "calibrated":calibrate}, response => {
           setNote(response.note);
         });
       }
     },
-    [webcamRef, calibrate, note]
+    [webcamRef]
   );
   
   useEffect(() => {
@@ -68,13 +70,36 @@ function App() {
       capture();
     }, 0);
     return () => clearInterval(interval);
-  }, [capture, note]);
+  }, [capture, note, webcamRef]);
 
   const videoConstraints = {
     width: 600,
     height: 600,
     facingMode: "user"
   };
+
+  const span = {
+    backgroundColor: "#E7F5FE",
+  };
+  
+  const Instruments = [
+    { label: <font face="verdana" color="black"> <span> Piano  </span> </font>, value: 1 },
+    { label: <font face="verdana" color="black">Violin</font>, value: 2 },
+    { label: <font face="verdana" color="black">Trumpet</font>, value: 3 },
+  
+  ];
+  
+  const droppy = () => (
+    <div className="app">
+      <div className="center">
+        <div style={{width:864}} align="center">
+          <Select options={Instruments} onChange={opt => console.log(opt.label, opt.value)}
+          />
+        </div>
+      </div>
+    </div>
+  
+  );
 
   const httpsPost = (data, callback) => {
     var post_options = {
@@ -104,11 +129,27 @@ function App() {
   return (
     <div style={styles.App}>
       <header style={styles.AppHeader}>
-        <div style={styles.Jumbo}>
-          <img src={logo} style={styles.AppLogo} alt="logo" />
-          <h1 style={styles.Heading}>Jazz Hands</h1>
-          <img src={logo} style={styles.AppLogo} alt="logo" />
-        </div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home"><h1>                </h1> </Navbar.Brand>
+          <div className="col-lg-4 col-lg-offset-5 col-xs-2 col-xs-offset-4">
+            <ul className=" nav navbar-nav ">
+              <li><a href="#"> </a></li>
+            </ul>
+          </div>  
+          <Navbar.Brand href="#home" style={styles.Jumbo}>
+            <h1> Jazz Hands </h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="Select an Instrument" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <div styles={styles.webcam}>
           <Webcam
             audio={false}
@@ -121,14 +162,16 @@ function App() {
         </div>
         <Button
           onClick={() => {
-            httpsPost({"image": webcamRef.current.getScreenshot(), "calibrated":calibrate}, response => {
+            httpsPost({"image": webcamRef.current.getScreenshot(), "calibrated":"false"}, response => {
               setCalibrate("true");
             });
           }}
           style={styles.Button}
           variant="primary" 
         >
-        Calibrate
+          <img src={logo} style={styles.AppLogo} alt="logo" />
+          <a style={styles.ButtonText}>Calibrate</a>
+          <img src={logo} style={styles.AppLogo} alt="logo" />
         </Button>
       </header>
     </div>
